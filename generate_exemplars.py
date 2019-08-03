eb696d6d039f18cbf8338ec9a4e8dc732432ab29
Roozbeh Pournader <roozbeh@unicode.org>
2019-08-03 06:39:12 +0200

    Expand generate_exemplars.py to generate more details and files

--- generate_exemplars.py
+++ generate_exemplars.py
@@ -5,6 +5,8 @@ import glob
 
 import common
 
+SEPARATE_BY_AGE = False
+
 
 def get_name(code):
     if name[code] is None:
@@ -29,28 +31,38 @@ def get_name(code):
         return name[code]
 
 
-def group_by_gc_and_sc(codes):
+def normalize_gc(category):
+    if category in ["Lu", "Ll", "Lt"]:
+        return "L&"
+    else:
+        return category
+
+
+def group_by_gc_and_sc_and_age(codes):
     codes = sorted(codes)
     last_start = last_end = codes[0]
     last_gc = gc[last_start]
     last_sc = sc[last_start]
+    last_age = age[last_start]
     for code in codes[1:]:
         if (
             code == last_end + 1
             and gc[code] == last_gc
             and sc.get(code, None) == last_sc
+            and (not SEPARATE_BY_AGE or age.get(code, None) == last_age)
         ):
             last_end = code
         else:
-            yield (last_start, last_end, last_gc, last_sc)
+            yield (last_start, last_end, last_gc, last_sc, last_age)
             last_start = last_end = code
             last_gc = gc[code]
             last_sc = sc.get(code, None)
-    yield (last_start, last_end, last_gc, last_sc)
+            last_age = age.get(code, None)
+    yield (last_start, last_end, last_gc, last_sc, last_age)
 
 
-def print_code_points(script, codes):
-    for start, end, category, sc in group_by_gc_and_sc(codes):
+def print_code_points_for_exemplars(output_file, script, codes):
+    for start, end, category, sc, ag in group_by_gc_and_sc_and_age(codes):
         if start == end:
             range_str = "%04X" % start
             name_str = get_name(start)
@@ -62,15 +74,34 @@ def print_code_points(script, codes):
         range_str = range_str.ljust(13)
         count_str = count_str.rjust(7)
         print(
-            "%s ; %s # gc=%s sc=%s %s %s"
+            "%s ; %s # %s %s %s%s %s"
             % (
                 script,
                 range_str,
                 category,
                 short_script_name.get(sc, "None"),
+                str(ag).rjust(4) + " " if SEPARATE_BY_AGE else "",
                 count_str,
                 name_str,
-            )
+            ),
+            file=output_file,
+        )
+
+
+def print_code_points_for_scx(scripts, codes):
+    for start, end, category in common.group_by_gc(codes, gc):
+        if start == end:
+            range_str = "%04X" % start
+            name_str = name[start]
+            count_str = ""
+        else:
+            range_str = "%04X..%04X" % (start, end)
+            name_str = "%s..%s" % (name[start], name[end])
+            count_str = "[%d]" % (end - start + 1)
+        range_str = range_str.ljust(13)
+        count_str = count_str.rjust(5)
+        print(
+            "%s ; %s # %s %s %s" % (range_str, scripts, category, count_str, name_str)
         )
 
 
@@ -87,10 +118,10 @@ with open("UnicodeData.txt") as uni_txt:
         elif char_name.endswith(", Last>"):
             for c in range(last_first, code + 1):
                 name[c] = None
-                gc[c] = char_gc
+                gc[c] = normalize_gc(char_gc)
         else:
             name[code] = char_name
-            gc[code] = char_gc
+            gc[code] = normalize_gc(char_gc)
 
 short_script_name = {}
 long_script_name = {}
@@ -110,6 +141,15 @@ for line in common.iterate_over_data_file("Scripts.txt"):
     for code in common.range_to_codes(codes):
         sc[code] = script
 
+age = {}
+for line in common.iterate_over_data_file("DerivedAge.txt"):
+    codes, char_age = line.split(";")
+    codes = codes.strip()
+    char_age = char_age.strip()
+
+    for code in common.range_to_codes(codes):
+        age[code] = char_age
+
 exemp = collections.defaultdict(set)
 for exemplar_filename in glob.glob("exemplars/*.txt"):
     script_code = exemplar_filename.split("/")[1].split(".")[0]
@@ -118,8 +158,31 @@ for exemplar_filename in glob.glob("exemplars/*.txt"):
             if code in name:
                 exemp[script_code].add(code)
 
-for script_code in sorted(exemp.keys()):
-    print("# " + long_script_name[script_code])
-    print()
-    print_code_points(script_code, exemp[script_code])
+with open("ScriptExemplars.txt", "wt") as exemp_file:
+    for script_code in sorted(exemp.keys()):
+        print("# " + long_script_name[script_code], file=exemp_file)
+        print(file=exemp_file)
+        print_code_points_for_exemplars(exemp_file, script_code, exemp[script_code])
+        print(file=exemp_file)
+
+
+scx = collections.defaultdict(set)
+for script_code in exemp:
+    for code in exemp[script_code]:
+        scx[code].add(script_code)
+
+scx_by_scripts = collections.defaultdict(set)
+for code in scx:
+    script_list = sorted(scx[code])
+    if len(script_list) == 1 and (
+        code not in sc or script_list[0] == short_script_name[sc[code]]
+    ):
+        continue
+    scx_by_scripts[" ".join(script_list)].add(code)
+
+script_lists = sorted(scx_by_scripts.keys(), key=common.script_set_to_sort_key)
+
+for script_list in script_lists:
+    print(script_list)
+    print_code_points_for_scx(script_list, scx_by_scripts[script_list])
     print()
